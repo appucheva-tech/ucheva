a927b077dbedb9c60981fda23726a4ff1aef735e
mohyis <mohyis007@gmail.com>
2026-06-20 05:33:12 +0100

    refactored staff controller

--- controller/staffController.js
+++ controller/staffController.js
@@ -14,7 +14,7 @@ exports.createStaff = async (req, res, next) => {
     try {
         const {id} = req.user
         const admin = await adminModel.findByPk(id)
-        const { firstName, lastName, otherName, gender, dateOfBirth, nationality, address, maritalStatus, staffType, staffRole, phoneNumber, email, qualification } = req.body;
+        const { firstName, lastName, otherName, gender, dateOfBirth, nationality, address, maritalStatus, staffType, phoneNumber, email, qualification } = req.body;
 
         // Check if the email is already in use
         const existingStaff = await staffModel.findOne({ where: { email } });
@@ -38,11 +38,9 @@ exports.createStaff = async (req, res, next) => {
             phoneNumber,
             email: email.toLowerCase().trim(),
             staffType,
-            staffRole,
             qualification,
             staffTokenExpiresAt: new Date(Date.now) + (60000 * 60 * 24)
         });
-        console.log(staff);
         
         const token = await jwt.sign({
             id: staff.id, email: staff.email}, 
