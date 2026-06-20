f5442c13f67cef0880ee1c83b0481e0064cf658b
offozoremmanuel <offozoremmy@gmail.com>
2026-06-20 08:20:11 +0100

    refactor payment initialization to include classId and className parameters; update Swagger documentation for clarity

--- controller/classController.js
+++ controller/classController.js
@@ -9,7 +9,7 @@ exports.assignOrCreateClass = async(req, res, next) =>{
         const admin = adminModel.findByPk(id)
         const { className, amount, installment, teacherId } = req.body
 
-         const checkClassExist = await classModel.findOne({where: {className: className}})
+         const checkClassExist = await classModel.findOne({where: {adminId: id, className: className}})
         
         if(checkClassExist){
             return res.status(400).json({
@@ -24,8 +24,6 @@ exports.assignOrCreateClass = async(req, res, next) =>{
                 message: 'invalid class amount'
             })
         }
-
-        const checkClassExist = await classModel.findOne({where: {adminId: id, className: className}})
         
         if(checkClassExist){
             return res.status(400).json({
