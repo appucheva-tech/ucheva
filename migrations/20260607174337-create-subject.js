7e8d16851f04dc83d245ba693996cde91d4febde
mohyis <mohyis007@gmail.com>
2026-06-21 17:09:30 +0100

    refactored subject model

--- migrations/20260607174337-create-subject.js
+++ migrations/20260607174337-create-subject.js
@@ -29,7 +29,7 @@ module.exports = {
         allowNull: false,
       },
       applicableClasses: {
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
         allowNull: false,
       },
       applicableDepartment:{
