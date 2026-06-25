09bbd8bf9f0fb47b038189abba4506ec4b774fb7
offozoremmanuel <offozoremmy@gmail.com>
2026-06-25 03:10:09 +0100

    add getStudentsByClass endpoint to retrieve students by class

--- router/studentRouter.js
+++ router/studentRouter.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { createStudent, getAllStudents, updateStudent, deleteStudent } = require('../controller/studentController');
+const { createStudent, getAllStudents, getStudentsByClass, updateStudent, deleteStudent } = require('../controller/studentController');
 const { authenticate, checkAdmin } = require('../middleware/authenticator');
 const { createStudentSchema, parentSettingsValidator } = require('../middleware/joiValidation');
 const upload = require('../middleware/multer');
 
 router.post('/student', checkAdmin, createStudentSchema, createStudent);
 router.get('/getAllStudents', checkAdmin, getAllStudents);
+router.get('/class/:classId', checkAdmin, getStudentsByClass);
 router.put('/updatestudent', checkAdmin, updateStudent)
 router.delete('/deletestudent', checkAdmin, deleteStudent)
 
