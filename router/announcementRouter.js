2e8a9b5afcea83e3693f1a2d1e9ce71ba2a5a77b
mohyis <mohyis007@gmail.com>
2026-06-28 11:46:06 +0100

    added get all announcement

--- router/announcementRouter.js
+++ router/announcementRouter.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
-const { checkAdmin } = require('../middleware/authenticator');
+const { checkAdmin, authenticate } = require('../middleware/authenticator');
 const {
     createAnnouncement,
     getAnnouncementDashboard,
     getAnnouncementByPk,
     deleteAnnouncement,
-    updateAnnoucement
+    updateAnnoucement,
+    getAllAnnouncement
 } = require('../controller/announcementController');
 
 router.get('/dashboard', checkAdmin, getAnnouncementDashboard);
+router.get('/recent-announcement', authenticate, getAllAnnouncement )
 router.post('/', checkAdmin, createAnnouncement);
 router.get('/:id', checkAdmin, getAnnouncementByPk)
 router.delete('/:id', checkAdmin, deleteAnnouncement)
