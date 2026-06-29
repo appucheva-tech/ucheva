bf15cdf3667b34423a1255c8cee2464f7fe4f1d2
mohyis <mohyis007@gmail.com>
2026-06-29 19:08:49 +0100

    refactored code

--- middleware/joiValidation.js
+++ middleware/joiValidation.js
@@ -204,6 +204,9 @@ exports.createClassValidator = validate(joi.object({
     paymentOption: joi.string().valid('full payment', 'installment').required().messages(messageMap('Payment option', {
         only: 'Payment option must be full payment or installment'
     })),
+    section: joi.string().valid('junior secondary', 'senior secondary').required().messages(messageMap('section', {
+        only: 'section must be junior secondary or senior secondary'
+    })),
     teacherId: uuid('Teacher ID').optional().allow(null),
     numberOfInstallments: joi.when('paymentOption', {
         is: 'installment',
